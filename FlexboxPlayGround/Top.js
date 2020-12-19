a4318a081ceaa4596dd6111159e281c79847d8cb
Jordan <jordanleelong5@gmail.com>
2020-12-18 21:01:25 -0800

    learning flexbox, went into code sandbox and started making shapes and positioning them

---
+++ FlexboxPlayGround/Top.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function Top() {
+  return (
+    <div className="top-container">
+      <div className="left-top">left top</div>
+      <div className="middle-top">middle top</div>
+      <div className="middle-top-two">second middle top</div>
+      <div className="right-top">right top</div>
+    </div>
+  );
+}
