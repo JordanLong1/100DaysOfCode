7f28240b437f53617c47e5f766d3d2c1ca8afdf2
Jordan <jordanleelong5@gmail.com>
2020-12-18 21:02:21 -0800

    added rails

---
+++ FlexboxPlayGround/LeftRail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function LeftRail() {
+  return (
+    <div className="left-rail-container">
+      <div className="left-left-rail">left left-rail</div>
+      <div className="middle-left-rail">middle left-rail</div>
+      <div className="middle-left-rail-two">second middle left-rail</div>
+      <div className="right-left-rail">right left-rail</div>
+    </div>
+  );
+}
---
+++ FlexboxPlayGround/RightRail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function RightRail() {
+  return (
+    <div className="right-rail-container">
+      <div className="left-right-rail">right right-rail</div>
+      <div className="middle-right-rail">middle right-rail</div>
+      <div className="middle-right-two">second middle right-rail</div>
+      <div className="right-rail">right right-rail</div>
+    </div>
+  );
+}
