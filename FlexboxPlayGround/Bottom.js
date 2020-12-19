2cfceba641a31c8e28cd75609104c1be4bdff376
Jordan <jordanleelong5@gmail.com>
2020-12-18 21:02:00 -0800

    bottom of flexbox fun

---
+++ FlexboxPlayGround/Bottom.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function Bottom() {
+  return (
+    <div className="bottom-container">
+      <div className="left-bottom">left bottom</div>
+      <div className="middle-bottom">middle bottom</div>
+      <div className="middle-bottom-two">second middle bottom</div>
+      <div className="right-bottom">right bottom</div>
+    </div>
+  );
+}
