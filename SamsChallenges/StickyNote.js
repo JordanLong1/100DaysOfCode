0e13b1e8fa720a986a1abafc671518304ebc2093
Jordan <jordanleelong5@gmail.com>
2020-12-29 16:56:25 -0800

    added mouse events and callbacks for them

--- SamsChallenges/StickyNote.js
+++ SamsChallenges/StickyNote.js
@@ -2,9 +2,39 @@ import React, {useState} from 'react';
 
 export default function StickyNote() {
 
+    const [translateCoords, setTranslateCoords] = useState({ x: 0, y: 0 });
+  const [dragStartCoords, setDragStartCoords] = useState(null);
+
+  const handleMouseDown = (e) => {
+    setDragStartCoords({
+        clientX: e.clientX, 
+        clientY: e.clientY,
+        translateX: translateCoords.x, 
+        translateY: translateCoords.y
+      });
+  }
+
+  const handleMouseMove = (e) => {
+    if (dragStartCoords) {
+        setTranslateCoords({
+          x: e.clientX - dragStartCoords.clientX + dragStartCoords.translateX, 
+          y: e.clientY - dragStartCoords.clientY + dragStartCoords.translateY
+        })
+    }
+  }
+
+  const handleMouseUp = () => {
+    setDragStartCoords(null)
+
+  }
+
     return (
-        <div>
-            StickyNote
+        <div className='sticky-note-container' 
+        onMouseDown={(e) => handleMouseDown(e)}
+        onMouseMove={(e) => handleMouseMove(e)}
+        onMouseUp={(e) => handleMouseUp(e)}
+        >
+           <textarea type='text' />
         </div>
     )
 }
