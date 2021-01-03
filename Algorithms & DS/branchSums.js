f4b56ce978a79cb94b1c09fb4e29ad36739ee573
Jordan <jordanleelong5@gmail.com>
2021-01-02 22:45:21 -0800

    added solution for branchSums

---
+++ Algorithms & DS/branchSums.js
@@ -0,0 +1,39 @@
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  function branchSums(root) {
+      let sumsList = []
+      return branchSumsHelper(root, 0, sumsList)
+  }
+  
+  function branchSumsHelper(root, sum, sumsList) {
+      if (!root) {
+          return;
+      }
+              
+      sum = sum + root.value
+      
+      if (root.left === null && root.right === null) {
+          sumsList.push(sum)
+      }
+      
+  
+      branchSumsHelper(root.left, sum, sumsList)
+      
+      
+      
+       branchSumsHelper(root.right, sum, sumsList)
+      
+      return sumsList
+      
+  }
+  
+  // Do not edit the lines below.
+  exports.BinaryTree = BinaryTree;
+  exports.branchSums = branchSums;
+  
