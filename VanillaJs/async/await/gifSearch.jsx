2a3ef78c9560c804610a5360ff80bbdc28a72050
Jordan <jordanleelong5@gmail.com>
2021-01-05 18:25:16 -0800

    made a gif search component using hooks

---
+++ VanillaJs/async/await/gifSearch.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react'
+
+function App() {
+
+  const [input, setInput] = useState(''); 
+  const [term, setTerm] = useState(''); 
+  const [results, setResults] = useState([]); 
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    setTerm(input)
+    setInput('')
+  }
+
+  useEffect(() => {
+
+    const fetchGifs = async () => {
+      
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=INSERT_YOUR_OWNq=${term}&limit=10&offset=0&rating=g&lang=en`)
+      const data = await response.json();
+      setResults(data.data.map(gif => gif.images.preview.mp4));
+    }
+    if (term !=='') {
+      fetchGifs()
+    }
+  }, [term])
+
+
+  return (
+    <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+      <form onSubmit={handleSubmit}>
+     <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Search for a Gif!' />
+    <button type='submit'>Submit</button>
+      {results.map(gif => <video autoPlay loop key={gif} src={gif} alt='' />)}
+      </form>
+    </div>
+  );
+}
+
+export default App;
