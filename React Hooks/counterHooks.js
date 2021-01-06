1f81125cbb4ae84591b4182b76ccd020568986d8
Jordan <jordanleelong5@gmail.com>
2021-01-05 22:54:54 -0800

    refactored so we are only getting local storage on the first render

--- React Hooks/counterHooks.js
+++ React Hooks/counterHooks.js
@@ -5,14 +5,16 @@ import React, {useState, useEffect} from 'react';
 
 export default function counterHooks() {
 
-    const initalCount = Number(window.localStorage.getItem('count') || 0);
-    const [count, setCount] = useState(initalCount); 
+    const getInitialCount = () =>  Number(window.localStorage.getItem('count') || 0);
+
+
+    const [count, setCount] = useState(getInitialCount);
 
     const incrementCount = () => setCount(count + 1);
     
     useEffect(() => {
         window.localStorage.setItem('count', count);
-    }, [])
+    }, [count])
 
     return <button onClick={() => setCount(incrementCount)}>{count}</button>
 }
