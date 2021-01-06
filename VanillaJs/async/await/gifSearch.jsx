2e7cbbc6b2130838d644e4c32cd5f90b117997bd
Jordan <jordanleelong5@gmail.com>
2021-01-05 18:48:37 -0800

    added abort controller and trying to learn try {} in the useEffect hook

--- VanillaJs/async/await/gifSearch.jsx
+++ VanillaJs/async/await/gifSearch.jsx
@@ -16,13 +16,24 @@ function App() {
   useEffect(() => {
 
     const fetchGifs = async () => {
+      try {
+      const abortController = new AbortController(); 
+      const abortSignal = {signal: abortController.signal}
       
-      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=INSERT_YOUR_OWNq=${term}&limit=10&offset=0&rating=g&lang=en`)
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=INSERT_YOUR_OWNq=${term}&limit=10&offset=0&rating=g&lang=en`, abortSignal)
       const data = await response.json();
+      
       setResults(data.data.map(gif => gif.images.preview.mp4));
+      } catch (error) {
+        new Error('Something went wrong')
+      }
     }
     if (term !=='') {
       fetchGifs()
+    } 
+
+    return () => {
+      abortController.abort(); 
     }
   }, [term])
 
