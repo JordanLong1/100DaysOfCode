0c13dcd6c077e1905c9236e49c9a8d8b93848b7a
Jordan <jordanleelong5@gmail.com>
2021-01-07 20:50:16 -0800

    did another refactor, added a size prop and added conditional for sizeClassName, commented every line

--- React Fundamentals/reusableBoxes.js
+++ React Fundamentals/reusableBoxes.js
@@ -1,21 +1,32 @@
+
 import * as React from 'react'
-import './box-styles.css'
+import '../box-styles.css'
 
 
-function Box({className = '', style, ...otherProps}) {
+function Box({className, size = '', style, ...otherProps}) {
+  // conditional to add box-- if there is a size prop passed down. 
+  const sizeClassName = size ? `box--${size}` : '' 
+  console.log(otherProps)
   return (
-   <div className={`box ${className}`} 
-   style={{fontStyle: 'italic', ...style}} 
-   {...otherProps} />
+   <div className={`box ${className} ${sizeClassName}`}  // return the div, template literal our className from the App component, as well as
+   // the size prop. 
+   style={{fontStyle: 'italic', ...style}} // set the font size to italic for all of the Box's, use the spread operator because we are
+   // overriding the style prop from the otherProps, and then we are overriding this style prop because of the otherProps (confusing but makes sense if you move it around)
+   // also, the order of styles matter. if i put ...style on the other side it would override the lightblue, pink, orange etc. 
+   {...otherProps} /> // in this case, other props is pretty much what wasnt specificed to be passed through here. 
+   // small lightblue box, medium pink box, large orange box.
+   
   )
 }
 
 function App() {
+  // passing down size, style & children props to Box.
   return (
     <div>
-      <Box className='box--small' style={{backgroundColor: 'lightblue'}} >small lightblue box</Box>
-      <Box className='box--medium' style={{backgroundColor: 'pink'}} >medium pink box</Box>
-      <Box className='box--large' style={{backgroundColor: 'orange'}} >large orange box</Box>
+      <Box size='small' style={{backgroundColor: 'lightblue'}} >small lightblue box</Box> 
+      <Box size='medium' style={{backgroundColor: 'pink'}} >medium pink box</Box>
+      <Box size='large' style={{backgroundColor: 'orange'}} >large orange box</Box>
+
     </div>
   )
 }
