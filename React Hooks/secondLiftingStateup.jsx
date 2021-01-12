0bfe2e2c3b9c50efa4b169de84d8fd496646de95
Jordan <jordanleelong5@gmail.com>
2021-01-11 18:19:55 -0800

    colocation refactor for performance and maintainability

--- React Hooks/secondLiftingStateup.jsx
+++ React Hooks/secondLiftingStateup.jsx
@@ -1,11 +1,13 @@
 
+
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input id="name" value={name} onChange={event => setName(event.target.value)} />
     </div>
   )
 }
@@ -25,18 +27,17 @@ function FavoriteAnimal({animal, onAnimalChange}) {
 
 
 
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({animal}) {
+  return <div>{`Hey, your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   const [animal, setAnimal] = React.useState('')
-  const [name, setName] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name />
       <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)} />
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
