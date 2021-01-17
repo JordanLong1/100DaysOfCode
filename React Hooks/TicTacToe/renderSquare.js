753d1556cf33f0bda858636b399a6faaa032e39f
Jordan <jordanleelong5@gmail.com>
2021-01-16 23:08:42 -0800

    rendered squares

---
+++ React Hooks/TicTacToe/renderSquare.js
@@ -0,0 +1,11 @@
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => selectSquare(i)}>
+        {squares[i]}
+      </button>
+    )
+  }
+  
+
+  export default renderSquare
