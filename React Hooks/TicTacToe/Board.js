9c7b63a2ff660247bbea4c8121e1697e47e343a5
Jordan <jordanleelong5@gmail.com>
2021-01-20 12:11:41 -0800

    added useLocalStorageState custom hook to handle setting and getting squares in local storage

--- React Hooks/TicTacToe/Board.js
+++ React Hooks/TicTacToe/Board.js
@@ -1,13 +1,8 @@
 import * as React from 'react'
-
+import {useLocalStorageState} from './utils'
 function Board() {
-  const [squares, setSquares] = React.useState(() => {
-    return JSON.parse(window.localStorage.getItem('squares') || Array(9).fill(null))
-  });
-
-  React.useEffect(() => {
-    window.localStorage.setItem('squares', JSON.stringify(squares))
-  }, [squares])
+ 
+  const [squares, setSquares] = useLocalStorageState('squares', Array(9).fill(null) )
 
   let nextValue = calculateNextValue(squares)
   let winner = calculateWinner(squares) 
